f1147e1cbac87edcdd3f97731fa24ea08c0be357
gwendall <gwendall.esnault@gmail.com>
2023-07-25 11:10:28 +0200

    polish

--- content.js
+++ content.js
@@ -1,21 +1,47 @@
 // content.js
-console.log('Mutation Observer started !!!');
 
 // Function to replace text when element is found
 async function replaceLogo() {
-  // Select all elements based on the CSS selector
-  let element = document.querySelector('header a[aria-label="Twitter"]');
 
+  // Get the URL of the SVG file
+  const svgURL = chrome.runtime.getURL("bird.svg");
+
+  // Fetch the SVG
+  const response = await fetch(svgURL);
+  const logoText = await response.text();
+
+  // Parse SVG string to Node
+  const parser = new DOMParser();
+  const logo = parser.parseFromString(logoText, "image/svg+xml").documentElement;
+  logo.setAttribute('width', "24");
+  logo.setAttribute('height', "24");
+
+  // Replace the logo
+  const element = document.querySelector('header a[aria-label="Twitter"]');
   if (element) {
-    // Get the URL of the SVG file
-    let svgURL = chrome.runtime.getURL("bird.svg");
-
-    // Fetch the SVG
-    let response = await fetch(svgURL);
-    let data = await response.text();
-
-    element.innerHTML = data;
+    element.innerHTML = logo.outerHTML;
   }
+
+  const elementToReplace = document.querySelector('svg[aria-label="Twitter"]');
+  if (elementToReplace) {
+    const logoClone = logo.cloneNode(true); // Clone the Node as we can't use the same Node twice
+    elementToReplace.parentNode.replaceChild(logoClone, elementToReplace);
+  }
+
+  const elementToReplace2 = document.querySelector('main > div > div > div > div > div > svg');
+  if (elementToReplace2) {
+    const logoClone = logo.cloneNode(true); // Clone the Node as we can't use the same Node twice
+    elementToReplace2.parentNode.replaceChild(logoClone, elementToReplace2);
+  }
+  
+}
+
+function replaceFavicon() {
+  const link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+  link.type = 'image/x-icon';
+  link.rel = 'shortcut icon';
+  link.href = chrome.runtime.getURL("favicon.ico");  // replace with the path to your favicon
+  document.getElementsByTagName('head')[0].appendChild(link);
 }
 
 // Options for the observer (which mutations to observe)
@@ -24,11 +50,18 @@ const config = { attributes: false, childList: true, subtree: true };
 // Callback function to execute when mutations are observed
 const callback = function(mutationsList, observer) {
   // Look through all mutations that just occured
-  for(let mutation of mutationsList) {
+  for (const mutation of mutationsList) {
     // If the addedNodes property has one or more nodes
-    for(let node of mutation.addedNodes) {
+    for (const node of mutation.addedNodes) {
       // Checks if the added node has the targeted element
-      if(node.querySelector && node.querySelector('header a[aria-label="Twitter"]')) {
+      if (
+        node.querySelector &&
+        (
+          node.querySelector('header a[aria-label="Twitter"]') ||
+          node.querySelector('svg[aria-label="Twitter"]') ||
+          node.querySelector('main > div > div > div > div > div > svg')
+        )
+      ) {
         replaceLogo();
         break;  // If the targeted element is found, stop the loop to prevent unnecessary checks
       }
@@ -41,3 +74,4 @@ const observer = new MutationObserver(callback);
 
 // Start observing the document with the configured parameters
 observer.observe(document.body, config);
+replaceFavicon();
